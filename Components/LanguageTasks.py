ec75d0347ac97b8fe003c1d27c7f7ac788bedf4a
ricky-aufvaa <aufvaa@gmail.com>
2025-09-29 20:02:01 +0530

    Improve transcription response handling

--- Components/LanguageTasks.py
+++ Components/LanguageTasks.py
@@ -48,7 +48,7 @@ IF YOU DONT HAVE ONE start AND end WHICH IS FOR THE LENGTH OF THE ENTIRE HIGHLIG
 """
 
 # User = """
-# 0.0 - 6.32:  Should we bring this up? I guess we have to so this just happened. We just found out that Charlie Kirk got shot9.0 - 11.48:  An awful and is he dead?14.0 - 19.92:  That's what was one of the guys out there just said okay firm in the lobby was just I was looking19.92 - 22.240000000000002:  I've been looking, I haven't seen anything that's confirmed.25.28 - 26.12:  Whoa.28.200000000000003 - 31.120000000000005:  Murder for having a different opinion from somebody else.31.120000000000005 - 31.880000000000003:  Yeah.31.880000000000003 - 33.68:  Different ideology from somebody else.35.040000000000006 - 35.6:  Yeah.35.6 - 36.44:  Yeah.36.44 - 38.32000000000001:  I mean, I don't know.38.32 - 43.88:  Beliefs that didn't align. Yep. I'm sorry. Yep. Rest in peace. We're gonna snooze. Oh45.08 - 51.88:  Jesus 27 years old maybe 30 even have a suspect. I don't know51.88 - 56.88:  So I don't, I don't, I'm literally trying to check it all on Twitter and it's all.56.88 - 57.88:  F***.57.88 - 58.88:  F***.58.88 - 59.88:  Nobody deserves it.59.88 - 60.88:  You didn't deserve that.60.88 - 61.88:  Nobody deserves that.
+# Example
 # """
 
 
@@ -71,9 +71,6 @@ def GetHighlight(Transcription):
     )
     chain = prompt |llm.with_structured_output(JSONResponse,method="function_calling")
     response = chain.invoke({"Transcription":Transcription})
-    # print(f"Start is {response.start}")
-    # print(f"end is {response.end}")
-    # print(f"highlight is {response.content}\n\n")
     Start,End = int(response.start), int(response.end)
     print(f"Start is {Start}")
     print(f"End is {End}\n\n")
